60f12e089322699ec139f054f9eae8637d2f7d18
Ali Watters <ali.watters@gmail.com>
2017-01-11 20:31:12 -0800

    wip auth app - completed video 72

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,15 +1,71 @@
 import React, {Component} from 'react';
-import {View} from 'react-native';
-import {Button, Card, CardSection} from './common';
+import {Text} from 'react-native';
+import firebase from 'firebase';
+import {Button, Card, CardSection, Input, Spinner} from './common';
 
 class LoginForm extends Component {
+  state = {email: '', password: '', error: '', loading: false};
+
+  onButtonPress() {
+    console.log(this.state);
+    this.setState({loading: true});
+    const {email, password} = this.state;
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(this.onLoginSuccess.bind(this))
+      .catch((error) => {
+        console.log('error', error);
+        firebase
+          .auth()
+          .createUserWithEmailAndPassword(email, password)
+          .then(this.onLoginSuccess.bind(this))
+          .catch(this.onLoginFail.bind(this));
+      });
+  }
+
+  onLoginSuccess() {
+    console.log('user');
+    this.setState({error: '', loading: false, email: '', password: ''});
+  }
+
+  onLoginFail(error) {
+    console.log('error', error);
+    this.setState({error: 'Authentication Failed', loading: false});
+  }
+
+  renderButton() {
+    if (this.state.loading) {
+       return <Spinner size='small'/>;
+    }
+
+    return <Button onPress={this.onButtonPress.bind(this)}>Login</Button>;
+  }
+
   render() {
     return (
       <Card>
-        <CardSection/>
-        <CardSection/>
         <CardSection>
-          <Button>Login</Button>
+          <Input
+            placeholder='name@example.com'
+            label='Email'
+            keyboardType='email-address'
+            value={this.state.email}
+            onChangeText={email => this.setState({email})}
+          />
+        </CardSection>
+        <CardSection>
+          <Input
+            secureTextEntry
+            label='Password'
+            placeholder='password'
+            value={this.state.password}
+            onChangeText={password => this.setState({password})}
+          />
+        </CardSection>
+        <Text>{this.state.error}</Text>
+        <CardSection>
+          {this.renderButton()}
         </CardSection>
       </Card>
     );
